73a61126d52746fa30d79c23431cb2605404fcf4
songjd <songjinda@rybbaby.com>
2016-12-06 11:47:52 +0800

    上拉加载 标签优化

--- src/components/Tab.vue
+++ src/components/Tab.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
-
-  </div>
+  <span>
+    <mt-badge size="small" type="primary" v-if="type === 'good'">{{ type | formatTab }}</mt-badge>
+    <mt-badge size="small" type="success" v-if="type === 'share'">{{ type | formatTab }}</mt-badge>
+    <mt-badge size="small" type="error" v-if="type === 'ask'">{{ type | formatTab }}</mt-badge>
+    <mt-badge size="small" type="warning" v-if="type === 'job'">{{ type | formatTab }}</mt-badge>
+  </span>
 </template>
-<style scoped>
-
-</style>
 <script>
-    export default{
-        data(){
-            return {}
-        },
-        components: {}
+  export default{
+    props: {
+      type: {
+        type: String,
+        required:true,
+        default: 'default'
+      }
     }
+  }
 </script>
--- src/views/Topics.vue
+++ src/views/Topics.vue
@@ -1,67 +1,179 @@
 <template>
   <div>
     <mt-navbar v-model="active" class="navbar">
-      <mt-tab-item id="all">全部</mt-tab-item>
-      <mt-tab-item id="good">精华</mt-tab-item>
-      <mt-tab-item id="share">分享</mt-tab-item>
-      <mt-tab-item id="ask">问答</mt-tab-item>
-      <mt-tab-item id="job">招聘</mt-tab-item>
+      <mt-tab-item
+        @click.native="tabSwitch('')"
+        id="all">
+        全部
+      </mt-tab-item>
+      <mt-tab-item
+        @click.native="tabSwitch('good')"
+        id="good">
+        精华
+      </mt-tab-item>
+      <mt-tab-item
+        @click.native="tabSwitch('share')"
+        id="share">
+        分享
+      </mt-tab-item>
+      <mt-tab-item
+        @click.native="tabSwitch('ask')"
+        id="ask">
+        问答
+      </mt-tab-item>
+      <mt-tab-item
+        @click.native="tabSwitch('job')"
+        id="job">
+        招聘
+      </mt-tab-item>
     </mt-navbar>
-    <mt-tab-container v-model="active">
+    <mt-tab-container
+      v-model="active"
+      v-infinite-scroll="loadMore"
+      infinite-scroll-disabled="loading"
+      infinite-scroll-distance="10">
       <mt-tab-container-item id="all">
         <mt-cell
+          class="cell"
           v-for="item in list.all"
           is-link>
             <span slot="title">
+              <tab :type="item.tab"></tab>
               {{ item.title }}
-             <mt-badge size="small" color="#888">{{ item.tab | formatTab }}</mt-badge>
             </span>
             <img slot="icon" v-lazy="item.author.avatar_url" width="24" height="24">
         </mt-cell>
+        <div class="text-center hidden" v-show="list.all.length <= 0">
+          暂无数据
+        </div>
       </mt-tab-container-item>
       <mt-tab-container-item id="good">
-        <mt-cell v-for="n in 5" title="tab-container 2"></mt-cell>
+        <mt-cell
+          class="cell"
+          v-for="item in list.good"
+          is-link>
+            <span slot="title">
+              <tab :type="item.tab"></tab>
+              {{ item.title }}
+            </span>
+          <img slot="icon" v-lazy="item.author.avatar_url" width="24" height="24">
+        </mt-cell>
+        <div class="text-center hidden" v-show="list.good.length <= 0">
+          暂无数据
+        </div>
       </mt-tab-container-item>
       <mt-tab-container-item id="share">
-        <mt-cell v-for="n in 7" title="tab-container 3"></mt-cell>
+        <mt-cell
+          class="cell"
+          v-for="item in list.share"
+          is-link>
+            <span slot="title">
+              <tab :type="item.tab"></tab>
+              {{ item.title }}
+            </span>
+          <img slot="icon" v-lazy="item.author.avatar_url" width="24" height="24">
+        </mt-cell>
+        <div class="text-center hidden" v-show="list.share.length <= 0">
+          暂无数据
+        </div>
       </mt-tab-container-item>
       <mt-tab-container-item id="ask">
-        <mt-cell v-for="n in 5" title="tab-container 2"></mt-cell>
+        <mt-cell
+          class="cell"
+          v-for="item in list.ask"
+          is-link>
+            <span slot="title">
+              {{ item.title }}
+              <tab :type="item.tab"></tab>
+            </span>
+          <img slot="icon" v-lazy="item.author.avatar_url" width="24" height="24">
+        </mt-cell>
+        <div class="text-center hidden" v-show="list.ask.length <= 0">
+          暂无数据
+        </div>
       </mt-tab-container-item>
       <mt-tab-container-item id="job">
-        <mt-cell v-for="n in 7" title="tab-container 3"></mt-cell>
+        <mt-cell
+          class="cell"
+          v-for="item in list.job"
+          is-link>
+            <span slot="title">
+              <tab :type="item.tab"></tab>
+              {{ item.title }}
+            </span>
+          <img slot="icon" v-lazy="item.author.avatar_url" width="24" height="24">
+        </mt-cell>
+        <div class="text-center hidden" v-show="list.job.length <= 0">
+          暂无数据
+        </div>
       </mt-tab-container-item>
     </mt-tab-container>
+    <div v-if="loading" class="list-loading">
+      <mt-spinner type="snake" color="#26a2ff"></mt-spinner>
+    </div>
   </div>
 </template>
-<style scoped>
+<style>
   .navbar{
     margin-bottom: 15px;
   }
 </style>
 <script>
-    export default{
-        data(){
-            return {
-              active: 'all',
-              list:{
-                all:[],
-                good:[],
-                share:[],
-                ask:[],
-                job:[]
-              }
-            }
-        },
-        methods: {
-          getTopics(tab) {
-            this.$http.get('topics').then((res) => {
-              this.list[tab] = res.body.data;
-            })
-          }
-        },
-        created() {
-          this.getTopics('all');
+  import Tab from './../components/Tab.vue';
+  import Vue from 'vue';
+  export default{
+    data(){
+      return {
+        active: 'all',
+        page: 1,
+        isLoad: false,
+        loading: false,
+        list: {
+          all: [],
+          good: [],
+          share:[],
+          ask: [],
+          job: []
         }
+      }
+    },
+    methods: {
+      loadMore() {
+        this.getTopics();
+      },
+      tabSwitch(tab) {
+        this.page = 1;
+        this.list[tab] = [];
+        this.getTopics(tab);
+      },
+      getTopics(tab = this.active) {
+        this.$http.get('topics',{
+          params: {
+            tab: tab,
+            page: this.page++
+          }
+        }).then((res) => {
+          let data = res.body.data;
+          if(data.length <= 0 ){
+            this.isLoad = true;
+          }
+          this.list[tab] = this.list[tab].concat(data);
+        })
+      }
+    },
+    components: {
+      Tab
+    },
+    created() {
+      console.log(Vue.http.interceptors);
+      Vue.http.interceptors.pop();
+      Vue.http.interceptors.push((request, next) => {
+        this.loading = true;
+        next(() => {
+          this.loading = false;
+        });
+      });
+      this.getTopics('all');
     }
+  }
 </script>
